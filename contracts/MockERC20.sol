a743169a4543eea27b5a6fab3308aa803ada1583
pisuthd <pisuth.dae@gmail.com>
2026-03-25 15:42:09 +0900

    feat(sc): use owner term for mock tokens and allows anyone to mint

--- contracts/MockERC20.sol
+++ contracts/MockERC20.sol
@@ -6,20 +6,19 @@ import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 /**
  * @title MockERC20
  * @notice Simple ERC20 token for testing purposes
- * @dev Minter role can mint new tokens
  */
 contract MockERC20 is ERC20 {
-    address public minter;
+    address public owner;
     
     constructor(
         string memory name,
         string memory symbol,
         uint256 initialSupply,
-        address _minter
+        address _owner
     ) ERC20(name, symbol) {
-        minter = _minter;
+        owner = _owner;
         if (initialSupply > 0) {
-            _mint(_minter, initialSupply);
+            _mint(_owner, initialSupply);
         }
     }
     
@@ -28,8 +27,7 @@ contract MockERC20 is ERC20 {
      * @param to Address to mint to
      * @param amount Amount to mint
      */
-    function mint(address to, uint256 amount) external {
-        require(msg.sender == minter, "Only minter can mint");
+    function mint(address to, uint256 amount) external { 
         _mint(to, amount);
     }
     
@@ -38,17 +36,16 @@ contract MockERC20 is ERC20 {
      * @param from Address to burn from
      * @param amount Amount to burn
      */
-    function burn(address from, uint256 amount) external {
-        require(msg.sender == minter, "Only minter can burn");
+    function burn(address from, uint256 amount) external { 
         _burn(from, amount);
     }
     
     /**
      * @notice Update minter address
-     * @param newMinter New minter address
+     * @param newOwner New minter address
      */
-    function setMinter(address newMinter) external {
-        require(msg.sender == minter, "Only minter can update");
-        minter = newMinter;
+    function setMinter(address newOwner) external {
+        require(msg.sender == owner, "Only minter can update");
+        owner = newOwner;
     }
 }
